4b3c6f9c5b8446fbc53d444c315f0ee8a93f1a92
maryursul <ursulmar@gmail.com>
2020-03-19 16:14:30 +0200

    Fix bug with routing , replace Login method to home controller

--- StudChoice/StudChoice1/Controllers/HomeController.cs
+++ StudChoice/StudChoice1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using StudChoice1.Models;
@@ -12,15 +13,21 @@ namespace StudChoice1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public readonly UserManager<IdentityUser> _userManager;
+        public readonly SignInManager<IdentityUser> _signInManager;
+        [BindProperty]
+        public InputModel Input { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        
+
+    public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View("Login");
+            return View();
         }
 
         public IActionResult Privacy()
@@ -28,7 +35,63 @@ namespace StudChoice1.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+
+
+                var user = new IdentityUser { UserName = Input.TransictionNumber };
+                var resultReg = _userManager.CreateAsync(user, Input.Password);
+                if (resultReg.IsCompletedSuccessfully)
+                {
+                    var result = _signInManager.PasswordSignInAsync(Input.TransictionNumber, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                    if (result.IsCompletedSuccessfully)
+                    {
+                        //loginModel._logger.LogInformation("User logged in.");
+                        return View("Index");
+                    }
+
+                    else
+                    {
+                        //ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                        return View();
+                    }
+
+                }
+                else
+                {
+                    var result = _signInManager.PasswordSignInAsync(Input.TransictionNumber, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                    if (result.IsCompletedSuccessfully)
+                    {
+                        //loginModel._logger.LogInformation("User logged in.");
+                        return LocalRedirect(returnUrl);
+                    }
+
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                        return View();
+                    }
+                }
+
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View();
+        }
+    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
