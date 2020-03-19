e32bfb1b73499fbc65cb67f7f00ccf023f0bba6b
Nazar <nazarsamar32@gmail.com>
2020-03-19 23:43:06 +0200

    added registration with admins confirmation

---
+++ StudChoice/StudChoice1/Models/RegisterModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StudChoice1.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string TransictionNumber { get; set; }
+    }
+}
