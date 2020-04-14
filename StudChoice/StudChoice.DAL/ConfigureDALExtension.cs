cefe8f7e20df5929dbf5b11a2390218f4fc898a4
Nazar <nazarsamar32@gmail.com>
2020-04-14 23:11:26 +0300

    fixed bug with migrations and updating database

--- StudChoice/StudChoice.DAL/ConfigureDALExtension.cs
+++ StudChoice/StudChoice.DAL/ConfigureDALExtension.cs
@@ -30,7 +30,7 @@ namespace StudChoice.DAL
             this IServiceCollection services,
             IConfiguration configuration)
         {
-            services.AddDbContext<StudChoiceContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<StudChoiceContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), x=>x.MigrationsAssembly("StudChoice")));
         }
 
         private static void ConfigureIdentity(this IServiceCollection services)
